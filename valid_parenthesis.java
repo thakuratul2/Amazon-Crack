a56c53e98b30d89749741eb6e69c85ce1aa60bcf
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-16 19:39:53 +0530

    Amazon

--- valid_parenthesis.java
+++ valid_parenthesis.java
@@ -1,5 +1,33 @@
 class valid_parenthesis{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public char getVal(char ch){
+        switch(ch){
+            case ')': return '(';
+            case ']': return '[';
+            case '}': return '{';
+            default: return ' ';
+        }
+    }
+    public boolean isValid(String s) {
+        String opening = "([{";
+        String closing = ")]}";
+
+        Stack<Character> st = new Stack<>();
+
+        for(char ch:s.toCharArray()){
+            if(opening.indexOf(ch)!=-1){
+                st.push(ch);
+            }else{
+                if(st.size()==0){
+                    return false;
+                }
+                char tmp = st.pop();
+
+                if(getVal(ch)!=tmp){
+                    return false;
+                }
+            }
+        }
+    
+    return st.size() == 0;
     }
 }
