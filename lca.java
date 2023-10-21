cfe78a38f2b8f9198e5c0489c83b6c84a8567a68
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-21 19:25:43 +0530

    Amazon

--- lca.java
+++ lca.java
@@ -11,8 +11,8 @@ public class lca {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return null;
      if(root == p || root == q) return root;
-TreeNode left =lowestCommonAncestor(root.left,p,q);       
-TreeNode right = lowestCommonAncestor(root.right,p,q);
+TreeNode left =lowestCommonAncestor(root.left,p,q);     //if p and q is less than root  
+TreeNode right = lowestCommonAncestor(root.right,p,q); // if p and q is more than root
         if(left != null && right != null) return root;
         return left == null? right: left;
     }
