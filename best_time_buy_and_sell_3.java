a01605cd93c31c87fea8c7076647a0a7f8b1c4dd
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-25 18:13:30 +0530

    Amazon

--- best_time_buy_and_sell_3.java
+++ best_time_buy_and_sell_3.java
@@ -16,6 +16,7 @@ public class best_time_buy_and_sell_3 {
 			buy1 = Math.max(buy1, -prices[i]);
 			sell1 = Math.max(sell1, buy1 + prices[i]);
 			buy2 = Math.max(buy2, sell1 - prices[i]);
+            
 			sell2 = Math.max(sell2, buy2 + prices[i]);
 		}
 		return sell2;
