e896ef5587fd46157a880eb156db50eaebe7caa5
thakuratul2 <pratapsinghatul111@gmail.com>
2023-11-04 20:15:20 +0530

    Amazon

---
+++ first_positive_missing.java
@@ -0,0 +1,30 @@
+public class first_positive_missing {
+    
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+    
+            for(int i = 0;i<n;i++){
+                if(nums[i] <= 0 || nums[i] > n){
+                    nums[i] = n + 1;
+                }
+            }
+            //convert here to negative number
+            for(int i = 0;i<n;i++){
+                int index = Math.abs(nums[i]);
+    
+                if(index > n) 
+                continue;
+                index--;
+                if(nums[index] > 0){
+                    nums[index] = -nums[index];
+                }
+            }
+            for(int i = 0;i<n;i++){
+                if(nums[i] > 0){
+                    return i + 1;
+                }
+            }
+    
+            return n + 1;
+        }
+}
